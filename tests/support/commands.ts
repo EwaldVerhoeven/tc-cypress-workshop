2023f9a60e88ad129349c0431caab40bc77f6d93
Ewald Verhoeven <ewaldverhoeven@hotmail.com>
2024-02-20 19:43:42 +0100

    fix: excercise 2 thingy

--- tests/support/commands.ts
+++ tests/support/commands.ts
@@ -26,12 +26,12 @@ Cypress.Commands.add("loginRpg", (username, password) => {
     cy.get("[data-testid='login-button']").should("be.visible").click();
     cy.get("input[type='email']")
       .should("be.visible")
-      .type("ewald@testcoders.nl")
-      .should("have.value", "ewald@testcoders.nl");
+      .type(username)
+      .should("have.value", username);
     cy.get("input[type='password']")
       .should("be.visible")
-      .type("securePassword123")
-      .should("have.value", "securePassword123");
+      .type(password)
+      .should("have.value", password);
     cy.get("button[type='submit']")
       .should("be.visible")
       .click()
