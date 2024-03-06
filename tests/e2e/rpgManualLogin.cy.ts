6f3a39f617beb602d35d3c187c0f3534bc2c6b06
Ewald Verhoeven <ewaldverhoeven@hotmail.com>
2024-03-06 13:33:02 +0100

    fix: hook

--- tests/e2e/rpgManualLogin.cy.ts
+++ tests/e2e/rpgManualLogin.cy.ts
@@ -1,5 +1,5 @@
 describe("My first test", () => {
-  before(function () {
+  beforeEach(function () {
     cy.login("ewald@testcoders.nl", "securePassword123");
   });
   it("successful manual login", function () {
