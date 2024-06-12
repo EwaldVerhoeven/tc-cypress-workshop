8c6e832bf1402585cfdafd65cca697b489c711c9
Ewald Verhoeven <ewaldverhoeven@hotmail.com>
2024-06-12 16:04:58 +0200

    Add network mocking examples

--- tests/e2e/playTheGame.cy.ts
+++ tests/e2e/playTheGame.cy.ts
@@ -5,36 +5,39 @@ describe("Game play", () => {
     cy.request("/api/builds?build=mage") // (random)request to simulate 'preparing' stuff like testing for a certain state
       .then((resp) => {
         expect(resp.status).to.eq(200);
+        console.log(resp);
       });
   });
 
   beforeEach(function () {
-    // Fixture API option 1
+    // Fixture access option 1
     cy.fixture("testdata.json").then(function (data) {
-      this.data = data;
+      this.testData = data;
     });
 
-    //Fixture API option 2
-    // cy.fixture("testdata.json").as("data");
+    //Fixture access option 2
+    cy.fixture("testdata.json").as("data");
 
-    cy.login("ewald@testcoders.nl", "securePassword123");
+    // cy.login("ewald@testcoders.nl", "securePassword123");
     cy.visit("/");
   });
 
   it("Select character", function () {
-    const { role } = this.data; // Fixture API option 2
+    // cy.intercept("GET", "/api/builds").as("builds"); // Intercept 1 - Spying
+    cy.intercept("GET", "api/builds", { fixture: "pumpitup.json" }); // Intercept 2 - Mocking response
+
+    const { role } = this.testData; // Required for Fixture option 2
 
     HomePage.clickPlayButton();
-
+    // cy.wait("@builds").its("response.statusCode").should("eq", 200); // Intercept 1 - waiting
     PlayPage.enterName(role.name);
     PlayPage.selectBuild(role.build);
-    PlayPage.clickStart();
+    
 
+    PlayPage.clickStart();
     GamePage.clickClickMeButton(5, true);
     GamePage.uploadFile("./tests/fixtures/testUpload.txt", true);
     GamePage.enterTextInput("Lorem Ipsum", true);
     GamePage.slideSlider(100, true);
   });
-
-  it("Play game", function () {});
 });
